90e92b5fa62d88c65a865ee6214a22a1eb8aca1b
laymanuel <laymanuel@protonmail.com>
2021-11-21 10:12:51 -0600

    compile for java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,10 @@ repositories {
 dependencies {
     compileOnly("com.destroystokyo.paper", "paper-api", "1.13.2-R0.1-SNAPSHOT")
 }
+
+tasks {
+    compileJava {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
+}

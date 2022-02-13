1572ced6b98e9b0ec94be89da1d88e260687538d
laymanuel <laymanuel@protonmail.com>
2022-02-12 18:38:24 -0600

    Use different method for detecting dupes, closes #2
    thank you for help from lynxplay and sulu from Paper.
    I do not think I could have fixed this issue without.
    
    Co-authored-by: Bjarne Koll <LynxPlay101@gmail.com>

--- src/main/java/dev/thoughtcrime/gravitycontrol/GravityListener.java
+++ src/main/java/dev/thoughtcrime/gravitycontrol/GravityListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.util.BoundingBox;
 import org.bukkit.util.Vector;
 
 import java.util.EnumSet;
@@ -18,7 +19,8 @@ import static com.destroystokyo.paper.MaterialSetTag.ANVIL;
 import static com.destroystokyo.paper.MaterialTags.CONCRETE_POWDER;
 
 public class GravityListener implements Listener {
-  private static final EnumSet<BlockFace> DUPE_ALLOWED_PORTAL_FACES = EnumSet.of(
+  private static final Vector ZERO = new Vector(0, 0, 0);
+  private static final EnumSet<BlockFace> BLOCK_FACES = EnumSet.of(
     BlockFace.NORTH,
     BlockFace.EAST,
     BlockFace.SOUTH,
@@ -34,31 +36,26 @@ public class GravityListener implements Listener {
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onEntityChangeBlock(final EntityChangeBlockEvent event) {
     if (!(event.getEntity() instanceof final FallingBlock falling) || event.getTo() == Material.AIR) return;
-    final Vector velocity = falling.getVelocity().clone();
+    final BoundingBox boundingBox = falling.getBoundingBox().expand(-0.01D);
 
-    for (BlockFace face : DUPE_ALLOWED_PORTAL_FACES) {
+    for (BlockFace face : BLOCK_FACES) {
       final Block relative = event.getBlock().getRelative(face);
       if (relative.getType() != Material.END_PORTAL) continue;
 
-      if ((face == BlockFace.NORTH && velocity.getZ() < 0D)
-        || (face == BlockFace.EAST && velocity.getX() > 0D)
-        || (face == BlockFace.SOUTH && velocity.getZ() > 0D)
-        || (face == BlockFace.WEST && velocity.getX() < 0D)) {
-        this.dupe(falling, velocity);
-        return;
+      if (relative.getBoundingBox().overlaps(boundingBox)) {
+        this.dupe(falling, face.getDirection());
       }
     }
   }
 
-  private void dupe(final FallingBlock falling, final Vector velocity) {
+  private void dupe(final FallingBlock falling, final Vector portalRelativeVector) {
     final Location location = falling.getLocation();
     final World world = location.getWorld();
     if (!this.plugin.config.worlds.contains("*") && !this.plugin.config.worlds.contains(world.getName()))
       return;
 
     final Material m = falling.getBlockData().getMaterial();
-    if (
-      (m == Material.SAND && !this.plugin.config.blocks.sand)
+    if ((m == Material.SAND && !this.plugin.config.blocks.sand)
         || (m == Material.RED_SAND && !this.plugin.config.blocks.redSand)
         || (ANVIL.isTagged(m) && !this.plugin.config.blocks.anvil)
         || (m == Material.DRAGON_EGG && !this.plugin.config.blocks.dragonEgg)
@@ -66,7 +63,14 @@ public class GravityListener implements Listener {
         || (CONCRETE_POWDER.isTagged(m) && !this.plugin.config.blocks.concretePowder)
     ) return;
 
-    world.spawnFallingBlock(falling.getLocation().add(velocity.clone().multiply(0.25)), falling.getBlockData())
+    final Vector velocity = falling.getVelocity();
+
+    if (velocity.equals(ZERO)) {
+      world.spawnFallingBlock(falling.getLocation().add(portalRelativeVector.multiply(0.25D).setY(0.05D)), falling.getBlockData());
+      return;
+    }
+
+    world.spawnFallingBlock(location.add(velocity.clone().multiply(0.25)), falling.getBlockData())
       .setVelocity(velocity.clone().multiply(this.plugin.config.horizontalCoefficient).setY(velocity.getY() * this.plugin.config.verticalCoefficient));
   }
 }
